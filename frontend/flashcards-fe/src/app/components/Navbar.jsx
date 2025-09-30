899780abbabd7236d163fcb4eb77bf8f578cd12c
mikewng <156055657+mikewng@users.noreply.github.com>
2025-09-30 17:35:48 -0400

    navbar addition changes

--- frontend/flashcards-fe/src/app/components/Navbar.jsx
+++ frontend/flashcards-fe/src/app/components/Navbar.jsx
@@ -1,19 +1,31 @@
 import "./Navbar.scss"
+import { useNavigationContext } from "../context/useNavigationContext";
 
 const Navbar = () => {
+    const { setNavState } = useNavigationContext();
+
     return (
         <nav className="fc-navbar-cpnt-wrapper">
             <div className="fc-navbar-content">
                 <div className="fc-navbar-brand">
                     <svg className="fc-navbar-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     </svg>
-                    <span className="fc-navbar-title">Flashcards</span>
+                    <span className="fc-navbar-title">Adaptive Flashcards</span>
                 </div>
                 <div className="fc-navbar-links">
-                    <a href="#" className="fc-nav-link">Home</a>
-                    <a href="#" className="fc-nav-link">Decks</a>
-                    <button className="fc-nav-button">Sign In</button>
-                    <button className="fc-nav-button">Register...</button>
+                    <div className="fc-nav-option" onClick={() => setNavState("Home")}>
+                        <div className="fc-nav-link">Home</div>
+                    </div>
+                    <div className="fc-nav-option">
+                        <div className="fc-nav-link">Create...</div>
+                    </div>
+                    <div className="fc-nav-option" onClick={() => setNavState("Decks")}>
+                        <div className="fc-nav-link">My Decks</div>
+                    </div>
+                    <div className="fc-nav-account-container">
+                        <button className="fc-nav-button">Sign In</button>
+                        <button className="fc-nav-button">Register...</button>
+                    </div>
                 </div>
             </div>
         </nav>
--- frontend/flashcards-fe/src/app/layout.js
+++ frontend/flashcards-fe/src/app/layout.js
@@ -5,7 +5,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <title>Mike Wang</title>
+        <title>Adaptive Flashcards</title>
       </head>
       <body>
         {children}
