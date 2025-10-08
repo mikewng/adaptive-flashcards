70f6c5401c4523bd3994cc11cee4f8bb46751333
mikewng <156055657+mikewng@users.noreply.github.com>
2025-10-08 02:46:15 -0400

    removed question and answer header

--- frontend/flashcards-fe/src/app/screens/cardmanagement/components/CardComponent.jsx
+++ frontend/flashcards-fe/src/app/screens/cardmanagement/components/CardComponent.jsx
@@ -5,11 +5,9 @@ const CardComponent = ({ card, onEdit, onDelete }) => {
         <div className="fc-card-item">
             <div className="fc-card-content">
                 <div className="fc-card-section">
-                    <label className="fc-card-label">Question</label>
                     <p className="fc-card-text">{card.question}</p>
                 </div>
                 <div className="fc-card-section">
-                    <label className="fc-card-label">Answer</label>
                     <p className="fc-card-text">{card.answer}</p>
                 </div>
                 <div className="fc-card-section">
