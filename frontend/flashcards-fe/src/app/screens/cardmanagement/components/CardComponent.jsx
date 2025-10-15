c721fb556d45f939c2d7e7a39917d40ef1dbe451
mikewng <156055657+mikewng@users.noreply.github.com>
2025-10-15 00:32:37 -0400

    difficulty css

--- frontend/flashcards-fe/src/app/screens/cardmanagement/components/CardComponent.jsx
+++ frontend/flashcards-fe/src/app/screens/cardmanagement/components/CardComponent.jsx
@@ -1,8 +1,18 @@
 import "./CardComponent.scss"
 
-const CardComponent = ({ card, onEdit, onDelete }) => {
+const getDifficultyClass = (accuracyRate) => {
+    const ranges = [
+        { max: Infinity, min: 71, class: "easy" },
+        { max: 70, min: 40, class: "medium" },
+        { max: 39, min: 0, class: "hard" }
+    ];
+
+    return ranges.find(r => accuracyRate >= r.min && accuracyRate <= r.max)?.class || "none";
+};
+
+const CardComponent = ({ card, onEdit, onDelete, onGetStats }) => {
     return (
-        <div className="fc-card-item">
+        <div className={`fc-card-item ${getDifficultyClass(card.accuracy_rate)}`}>
             <div className="fc-card-content">
                 <div className="fc-card-section">
                     <p className="fc-card-text">{card.question}</p>
@@ -25,6 +35,7 @@ const CardComponent = ({ card, onEdit, onDelete }) => {
                     <div className="fc-card-actions">
                         <button
                             className="fc-card-action-btn fc-edit-btn"
+                            // onClick={() => onGetStats(card)}
                             title="Stats"
                         >
                             Stats
